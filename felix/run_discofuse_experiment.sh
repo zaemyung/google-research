70dc52d65f55158a300fe948e7c196c25dccf322
zaemyung <zaemyung@gmail.com>
2021-10-28 15:12:44 +0200

    update exp settings

--- felix/run_discofuse_experiment.sh
+++ felix/run_discofuse_experiment.sh
@@ -14,15 +14,16 @@
 
 #!/bin/bash
 # Please update these paths.
-export OUTPUT_DIR=/path/to/output
+export OUTPUT_DIR=/tmp-network/user/zaemyung-kim/projects/felix_exp
 # BERT can be found at https://storage.googleapis.com/cloud-tpu-checkpoints/bert/keras_bert/uncased_L-12_H-768_A-12.tar.gz
 # (Access to Pretrained Checkpoints, not Pretrained hub modules)
 # Note: only used uncased if do_lower_case=True. Furthermore, you will need to
 # change the felix_config.json to take into account the different vocab sizes.
-export BERT_BASE_DIR=/path/to/uncased_L-12_H-768_A-12
+export BERT_BASE_DIR=/tmp-network/user/zaemyung-kim/models/BERT/uncased_L-12_H-768_A-12
 # Requires untaring the downloaded file.
 # DiscoFuse can be found at https://github.com/google-research-datasets/discofuse
-export DISCOFUSE_DIR=/path/to/discofuse
+export DISCOFUSE_DIR=/tmp-network/user/zaemyung-kim/data/discofuse/discofuse_v1/wikipedia
+export FELIX_CONFIG_DIR=/tmp-network/user/zaemyung-kim/google-research/felix/discofuse
 export PREDICTION_FILE=${OUTPUT_DIR}/pred.tsv
 # If you wish to use another dataset please switch from input_format=discofuse
 # to wikisplit. wikisplit expects tab seperated source target pairs.
@@ -30,104 +31,106 @@ export PREDICTION_FILE=${OUTPUT_DIR}/pred.tsv
 # If False FelixInsert is used.
 export USE_POINTING='True'
 
+# Need to clone: git clone https://github.com/tensorflow/models.git
+export PYTHONPATH=$PYTHONPATH:/tmp-network/user/zaemyung-kim/google-models
 
-# Label map construction
-echo "Constructing vocabulary"
-python phrase_vocabulary_constructor_main \
---output="${OUTPUT_DIR}/label_map.json" \
---use_pointing="${USE_POINTING}" \
---do_lower_case="True"
 
-# Preprocess
-echo "Preprocessing data"
-python preprocess_main \
-  --input_file="${DISCOFUSE_DIR}/train.tsv" \
-  --input_format="discofuse" \
-  --output_file="${OUTPUT_DIR}/train.tfrecord" \
-  --label_map_file="${OUTPUT_DIR}/label_map.json" \
-  --vocab_file="${BERT_BASE_DIR}/vocab.txt" \
-  --do_lower_case="True" \
-  --use_open_vocab="True" \
-  --max_seq_length="128" \
-  --use_pointing="${USE_POINTING}" \
-  --split_on_punc="True"
+# # Label map construction
+# echo "Constructing vocabulary"
+# python phrase_vocabulary_constructor_main.py \
+# --output="${OUTPUT_DIR}/label_map.json" \
+# --use_pointing="${USE_POINTING}" \
+# --do_lower_case="True"
 
-python preprocess_main.py \
-  --input_file="${DISCOFUSE_DIR}/tune.tsv" \
-  --input_format="discofuse" \
-  --output_file="${OUTPUT_DIR}/tune.tfrecord" \
-  --label_map_file="${OUTPUT_DIR}/label_map.json" \
-  --vocab_file="${BERT_BASE_DIR}/vocab.txt" \
-  --do_lower_case="True" \
-  --use_open_vocab="True" \
-  --max_seq_length="128" \
-  --use_pointing="${USE_POINTING}" \
-  --split_on_punc="True"
+# # Preprocess
+# echo "Preprocessing data"
+# python preprocess_main.py \
+#   --input_file="${DISCOFUSE_DIR}/train.tsv" \
+#   --input_format="discofuse" \
+#   --output_file="${OUTPUT_DIR}/train.tfrecord" \
+#   --label_map_file="${OUTPUT_DIR}/label_map.json" \
+#   --vocab_file="${BERT_BASE_DIR}/vocab.txt" \
+#   --do_lower_case="True" \
+#   --use_open_vocab="True" \
+#   --max_seq_length="128" \
+#   --use_pointing="${USE_POINTING}" \
+#   --split_on_punc="True"
+
+# python preprocess_main.py \
+#   --input_file="${DISCOFUSE_DIR}/dev.tsv" \
+#   --input_format="discofuse" \
+#   --output_file="${OUTPUT_DIR}/dev.tfrecord" \
+#   --label_map_file="${OUTPUT_DIR}/label_map.json" \
+#   --vocab_file="${BERT_BASE_DIR}/vocab.txt" \
+#   --do_lower_case="True" \
+#   --use_open_vocab="True" \
+#   --max_seq_length="128" \
+#   --use_pointing="${USE_POINTING}" \
+#   --split_on_punc="True"
 
 # Train
 echo "Training tagging model"
 rm -rf "${OUTPUT_DIR}/model_tagging"
 mkdir -p "${OUTPUT_DIR}/model_tagging"
 
-python run_felix \
+python run_felix.py \
     --train_file="${OUTPUT_DIR}/train.tfrecord" \
-    --eval_file="${OUTPUT_DIR}/tune.tfrecord" \
+    --eval_file="${OUTPUT_DIR}/dev.tfrecord" \
     --model_dir_tagging="${OUTPUT_DIR}/model_tagging" \
-    --bert_config_tagging="${DISCOFUSE_DIR}/felix_config.json" \
+    --bert_config_tagging="${FELIX_CONFIG_DIR}/felix_config.json" \
     --max_seq_length=128 \
-    --num_train_epochs=500 \
-    --num_train_examples=8 \
-    --num_eval_examples=8 \
+    --num_train_epochs=100 \
+    --num_train_examples=16307647 \
+    --num_eval_examples=168060 \
     --train_batch_size="32" \
     --eval_batch_size="32" \
     --log_steps="100" \
     --steps_per_loop="100" \
     --train_insertion="False" \
     --use_pointing="${USE_POINTING}" \
-    --init_checkpoint="${BERT_DIR}/bert_model.ckpt" \
+    --init_checkpoint="${BERT_BASE_DIR}/bert_model.ckpt" \
     --learning_rate="0.00003" \
     --pointing_weight="1" \
     --use_weighted_labels="True" \
-    --init_checkpoint="${BERT_BASE_DIR}"
 
-echo "Training insertion model"
-rm -rf "${DATA_DIRECTORY}/model_insertion"
-mkdir "${DATA_DIRECTORY}/model_insertion"
-python run_felix \
-    --train_file="${OUTPUT_DIR}/train.tfrecord.ins" \
-    --eval_file="${OUTPUT_DIR}/tune.tfrecord.ins" \
-    --model_dir_insertion="${OUTPUT_DIR}/model_insertion" \
-    --bert_config_insertion="${DISCOFUSE_DIR}/felix_config.json" \
-    --max_seq_length=128 \
-    --num_train_epochs=500 \
-    --num_train_examples=8 \
-    --num_eval_examples=8 \
-    --train_batch_size="32" \
-    --eval_batch_size="32" \
-    --log_steps="100" \
-    --steps_per_loop="100" \
-    --train_insertion="False" \
-    --init_checkpoint="${BERT_DIR}/bert_model.ckpt" \
-    --use_pointing="${USE_POINTING}" \
-    --learning_rate="0.00003" \
-    --pointing_weight="1" \
-    --train_insertion="True"
-
-# Predict
-echo "Generating predictions"
-
-python predict_main \
---input_format="discofuse" \
---predict_input_file="${DISCOFUSE_DIR}/test.tsv" \
---predict_output_file="${PREDICTION_FILE}"\
---label_map_file="${OUTPUT_DIR}/label_map.json" \
---vocab_file="${BERT_BASE_DIR}/vocab.txt" \
---max_seq_length=128 \
---predict_batch_size=32 \
---do_lower_case="True" \
---use_open_vocab="True" \
---bert_config_tagging="${DISCOFUSE_DIR}/felix_config.json" \
---bert_config_insertion="${DISCOFUSE_DIR}/felix_config.json" \
---model_tagging_filepath="${OUTPUT_DIR}/model_tagging" \
---model_insertion_filepath="${OUTPUT_DIR}/model_insertion" \
---use_pointing="${USE_POINTING}"
+# echo "Training insertion model"
+# rm -rf "${DATA_DIRECTORY}/model_insertion"
+# mkdir "${DATA_DIRECTORY}/model_insertion"
+# python run_felix.py \
+#     --train_file="${OUTPUT_DIR}/train.tfrecord.ins" \
+#     --eval_file="${OUTPUT_DIR}/dev.tfrecord.ins" \
+#     --model_dir_insertion="${OUTPUT_DIR}/model_insertion" \
+#     --bert_config_insertion="${FELIX_CONFIG_DIR}/felix_config.json" \
+#     --max_seq_length=128 \
+#     --num_train_epochs=100 \
+#     --num_train_examples=16307647 \
+#     --num_eval_examples=168060 \
+#     --train_batch_size="32" \
+#     --eval_batch_size="32" \
+#     --log_steps="100" \
+#     --steps_per_loop="100" \
+#     --train_insertion="False" \
+#     --init_checkpoint="${BERT_BASE_DIR}/bert_model.ckpt" \
+#     --use_pointing="${USE_POINTING}" \
+#     --learning_rate="0.00003" \
+#     --pointing_weight="1" \
+#     --train_insertion="True"
+#
+# # Predict
+# echo "Generating predictions"
+#
+# python predict_main \
+# --input_format="discofuse" \
+# --predict_input_file="${DISCOFUSE_DIR}/test.tsv" \
+# --predict_output_file="${PREDICTION_FILE}"\
+# --label_map_file="${OUTPUT_DIR}/label_map.json" \
+# --vocab_file="${BERT_BASE_DIR}/vocab.txt" \
+# --max_seq_length=128 \
+# --predict_batch_size=32 \
+# --do_lower_case="True" \
+# --use_open_vocab="True" \
+# --bert_config_tagging="${FELIX_CONFIG_DIR}/felix_config.json" \
+# --bert_config_insertion="${FELIX_CONFIG_DIR}/felix_config.json" \
+# --model_tagging_filepath="${OUTPUT_DIR}/model_tagging" \
+# --model_insertion_filepath="${OUTPUT_DIR}/model_insertion" \
+# --use_pointing="${USE_POINTING}"
